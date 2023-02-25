046a7d5ebf5af76fb2cb802a547113bf4bf4c068
ajin3830 <ajin3830@gmail.com>
2023-02-25 15:59:17 -0500

    complete lab/ notes on  __init__() method and its args, self, default values

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed = 'Mutt'):
+        self.name = name
+        self.breed = breed
+
+    def show_self(self):
+        return self
+    
+# Dog takes a name as an argument and saves it to self.name 
+# Dog takes a breed as an argument and saves it to self.breed                                                                                 [ 50%]
+# Dog sets self.breed = "Mutt" when no breed specified 
+
+# fido = Dog('Fido')
+# fido.breed
+# snoopy = Dog('Snoopy', 'Husky')
+# snoopy.breed
+
+# fido.show_self()
+# fido is fido.show_self()
